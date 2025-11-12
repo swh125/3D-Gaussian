3c7448dd80064cd607092a480bfc8b61dd1e3f53
shuwen <121010025@link.cuhk.edu.cn>
2025-11-12 19:31:47 +0800

    Handle non-contiguous tensors in PSNR/MSE by using reshape

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -12,8 +12,8 @@
 import torch
 
 def mse(img1, img2):
-    return (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
+    return (((img1 - img2)) ** 2).reshape(img1.shape[0], -1).mean(1, keepdim=True)
 
 def psnr(img1, img2):
-    mse = (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
+    mse = (((img1 - img2)) ** 2).reshape(img1.shape[0], -1).mean(1, keepdim=True)
     return 20 * torch.log10(1.0 / torch.sqrt(mse))
